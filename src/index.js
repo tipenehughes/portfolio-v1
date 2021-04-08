2492676cc6951318bd7abb56ef1af5bbdc34187a
Tipene Hughes <tipenehughes@gmail.com>
2021-04-08 18:44:43 -0400

    Added datadog rum

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import { ThemeProvider } from "./context/ThemeContext";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { datadogRum } from "@datadog/browser-rum";
+
+datadogRum.init({
+    applicationId: "45017602-17b9-4f91-bba2-c186a1225e5b",
+    clientToken: "pub1bf993aa27dbae22507c5dd773c4c6bf",
+    site: "datadoghq.com",
+    service: "portfolio",
+    env: "prod",
+    // Specify a version number to identify the deployed version of your application in Datadog
+    // version: '1.0.0',
+    sampleRate: 100,
+    trackInteractions: true,
+});
+
 ReactDOM.render(
     <React.StrictMode>
         <ThemeProvider>
